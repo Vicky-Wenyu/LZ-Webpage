06e66fadc3978b50b5d0279664fd5c0d4c64b1dc
Huang Vicky <Vicky.Huang@alderhey.nhs.uk>
2025-02-13 09:53:32 +0000

    add print statement

--- server.py
+++ server.py
@@ -3,15 +3,19 @@ from flask import Flask, render_template, request
 import os
 import requests
 import smtplib
+import logging
 
 load_dotenv('Lazy Zebra Webpage\.env')
 
 def send_email(name, email, message):
-  recipient_email = 'geyerkristoffer@gmail.com'
+  recipient_email = 'vicky.huangwy@gmail.com'
   with smtplib.SMTP('smtp.gmail.com') as server:
     email_body = "subject:Lazy Zebra Message!\n\n" + f"{name} is tring to contact you, please email back at {email}. Here is your message '{message}'"+ "\n From Vicky" 
+    sender_email = os.environ.get("SENDER_EMAIL")
+    sender_pw = os.environ.get('PASSWORD')
+    print(f"Executed: fetching sender email & password{sender_email,sender_pw})")
     server.starttls()
-    server.login(os.environ.get("SENDER_EMAIL"), os.environ.get('PASSWORD'))
+    server.login(sender_email, sender_pw)
     server.sendmail(os.environ.get("SENDER_EMAIL"), recipient_email, email_body)
 
 app = Flask(__name__)
@@ -21,6 +25,7 @@ app.send_email = send_email
 def home_page ():
   blog_request = requests.get('https://api.npoint.io/29e74705f4ce2aa5ff49')
   all_blogs =  blog_request.json()
+  print('visited home page')
   return render_template("index.html", all_blogs = all_blogs)
 
 @app.route('/blog/<id>')
@@ -41,6 +46,7 @@ def form_data ():
     name = request.form["fullname"]
     email = request.form["email"]
     message = request.form["message"]
+    print(f"Executed: fetching name email and message successfully {name, email, message})")
     app.send_email(name, email, message)
     print(f'email sent')
     return f'<h1> Message sent successfully! </h1>'
