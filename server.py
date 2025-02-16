8f93dd648847a473660acf9b421bf5f95797e987
Huang Vicky <Vicky.Huang@alderhey.nhs.uk>
2025-02-16 19:28:01 +0000

    Use SengGrid for email

--- server.py
+++ server.py
@@ -3,22 +3,38 @@ from flask import Flask, render_template, request
 import os
 import requests
 import smtplib
-import logging
+from sendgrid import SendGridAPIClient
+from sendgrid.helpers.mail import Mail
+
 
 load_dotenv('Lazy Zebra Webpage\.env')
 
+''' 
+#previouse email function#
+
 def send_email(name, email, message):
-  print('send email function called successfully')
-  recipient_email = 'vicky.huangwy@gmail.com'
+  recipient_email = os.environ.get("RECIPIENT_EMAIL")
   with smtplib.SMTP('smtp.gmail.com') as server:
-    print ('connected to smtp server')
     email_body = "subject:Lazy Zebra Message!\n\n" + f"{name} is tring to contact you, please email back at {email}. Here is your message '{message}'"+ "\n From Vicky" 
     sender_email = os.environ.get("SENDER_EMAIL")
     sender_pw = os.environ.get('PASSWORD')
-    print(f"Executed: fetching sender email & password{sender_email,sender_pw})")
     server.starttls()
     server.login(sender_email, sender_pw)
     server.sendmail(os.environ.get("SENDER_EMAIL"), recipient_email, email_body)
+
+'''
+def send_email(name, email, message_from_webpage):
+      message = Mail(
+      from_email= os.environ.get("SENDER_EMAIL"),
+      to_emails= os.environ.get("RECIPIENT_EMAIL"),
+      subject='Lazy Zebra Message!',
+      html_content= f"{name} is tring to contact you, please email back at {email}. Here is your message '{message_from_webpage}'"+ " From Vicky")
+      api_key = os.environ.get('SENDGRID_API_KEY')
+      sg = SendGridAPIClient(api_key)
+      response = sg.send(message)
+      print(response.status_code)
+      print(response.body)
+      print(response.headers)
 
 app = Flask(__name__)
 app.send_email = send_email
